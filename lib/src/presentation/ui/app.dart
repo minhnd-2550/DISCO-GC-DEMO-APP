aa2b9f4b160070f179ced912b97d49f952033976
Quang Huy <doan.quang.huy@sun-asterisk.com>
2023-06-29 16:55:07 +0700

    Implement seminar enter screen

--- lib/src/presentation/ui/app.dart
+++ lib/src/presentation/ui/app.dart
@@ -8,6 +8,7 @@ import 'package:flutter_clean_architecture/src/presentation/ui/confirmation/conf
 import 'package:flutter_clean_architecture/src/presentation/ui/theme/theme.dart';
 import 'package:flutter_clean_architecture/src/presentation/ui/step/step_page.dart';
 import 'package:flutter_clean_architecture/src/presentation/ui/top/top_page.dart';
+import 'package:flutter_clean_architecture/src/presentation/ui/seminar/seminar_enter.dart';
 import 'package:flutter_gen/gen_l10n/res.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'detail/detail_page.dart';
@@ -39,6 +40,7 @@ class Application extends ConsumerWidget {
         return const Locale('en', '');
       },
       routes: {
+        seminarEnterPage: (_) => const SeminarEnterPage(),
         topPageRoutes: (_) => const TopPage(),
         stepPageRoutes: (_) => const StepPage(),
         detailPageRoutes: (_) => const DetailPage(),
